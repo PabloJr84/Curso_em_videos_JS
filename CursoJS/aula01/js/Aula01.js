a92135ad1acee2254d0faac9b6ebdc3659f2c47d
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-04-14 12:33:06 -0300

    3ªAtualização do curso JS

--- CursoJS/aula01/js/Aula01.js
+++ CursoJS/aula01/js/Aula01.js
@@ -1,3 +1,3 @@
-window.alert('Minhas primeira mensagem')
-window.confirm('Esta gostando da aula de JS ?')
-window.prompt('Qual é seu nome?')
+window.alert('Minhas primeira mensagem')// mensagem de alerta
+window.confirm('Esta gostando da aula de JS ?') // Janela com botão de ok e cancelar
+window.prompt('Qual é seu nome?') // Vai perguntar o nome
