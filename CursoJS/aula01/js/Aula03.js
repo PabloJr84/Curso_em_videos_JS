1cb2dcec6e7024261bbb36b091ceff81bee507c6
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-04-15 01:06:22 -0300

    5ª Taualização do curso em videos JS

---
+++ CursoJS/aula01/js/Aula03.js
@@ -0,0 +1,4 @@
+var n1 = Number.parseFloat (window.prompt('Digite um numero:'))// onde está Number.parsefloat pode ser usado somente o Number que ele entende.
+var n2 = Number.parseFloat( window.prompt('Digite outro nome:'))
+var soma = n1 + n2
+window.alert (' A soma dos numeros são , ' + soma)
