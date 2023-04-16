8d3f99eca2bac559278b7d058e2aaed783a98bbf
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-04-16 18:52:31 -0300

    Atualização de comentarios

--- CursoJS/aula01/js/Aula04.js
+++ CursoJS/aula01/js/Aula04.js
@@ -1,5 +1,6 @@
 var nome = window.prompt('Qual é seu nome ?')
-document.write(`Seu nome tem ${nome.length} letras.<br/>`)
-document.write(`Seu nome em letra maiúsculas é ${nome.toUpperCase()} <br/>`)
-document.write(`Seu nom em minúsculas é ${nome.toLocaleLowerCase()}`)
+document.write(`Seu nome tem ${nome.length} letras.<br/>`)/*quando caracteres a string tem*/ 
+document.write(`Seu nome em letra maiúsculas é ${nome.toUpperCase()} <br/>`) /*muda tudo para maiúsculas*/
+document.write(`Seu nom em minúsculas é ${nome.toLocaleLowerCase()}`) /*muda todas as letras para minusculas*/
+
 
