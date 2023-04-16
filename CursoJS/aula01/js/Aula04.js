7ebeb20c07d946c2502d37082cf2f0503ddd2dd9
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-04-16 18:47:42 -0300

    7ª Atualização do curso em video JS

---
+++ CursoJS/aula01/js/Aula04.js
@@ -0,0 +1,5 @@
+var nome = window.prompt('Qual é seu nome ?')
+document.write(`Seu nome tem ${nome.length} letras.<br/>`)
+document.write(`Seu nome em letra maiúsculas é ${nome.toUpperCase()} <br/>`)
+document.write(`Seu nom em minúsculas é ${nome.toLocaleLowerCase()}`)
+
